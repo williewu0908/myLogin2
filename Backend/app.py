e1ad6730d35b430cc8028f9ba1e37b2726cc18b1
WillieWu <itri464687@itri.org.tw>
2025-11-27 11:22:21 +0800

    Modify:mail use ssl

--- Backend/app.py
+++ Backend/app.py
@@ -37,6 +37,7 @@ app.config['MAIL_DEBUG'] = True
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = app.config['MAIL_USERNAME']
